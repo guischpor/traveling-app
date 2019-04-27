7c6aaff80b5c495c7541d1b5773bc92649789f92
guilhermeportela <38728633+guischpor@users.noreply.github.com>
2019-04-27 16:35:56 -0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:traveling_app/screens/login_screen.dart';
 
-void main() => runApp(LoginScreen());
+void main() {
+  runApp(MaterialApp(
+    title: 'Travelling',
+    home: LoginScreen(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,16 +1,114 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
-  LoginScreen({Key key}) : super(key: key);
+  const LoginScreen({Key key}) : super(key: key);
 
+  @override
   _LoginScreenState createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  GlobalKey<FormState> _formKey = GlobalKey();
+
+  final _emailController = TextEditingController();
+  final _senhaController = TextEditingController();
+
+  bool _validate = false;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
+    final Color primaryColor = Theme.of(context).primaryColor;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+        centerTitle: true,
+        actions: <Widget>[
+          FlatButton(
+            child: Text('CRIAR CONTA',
+                style: TextStyle(
+                  fontSize: 15.0,
+                )),
+            textColor: Colors.white,
+            onPressed: () {
+              // Navigator.of(context).pushReplacement(
+              //     MaterialPageRoute(builder: (context) => SignupScreen()));
+            },
+          ),
+        ],
+      ),
+      body: Form(
+        autovalidate: _validate,
+        key: _formKey,
+        child: ListView(
+          padding: EdgeInsets.all(16.0),
+          children: <Widget>[
+            TextFormField(
+              decoration: InputDecoration(hintText: 'E-mail'),
+              keyboardType: TextInputType.emailAddress,
+              validator: _validateEmail,
+              controller: _emailController,
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            TextFormField(
+              decoration: InputDecoration(hintText: 'Senha'),
+              obscureText: true,
+              validator: _validateSenha,
+              controller: _senhaController,
+            ),
+            Align(
+                alignment: Alignment.centerRight,
+                child: FlatButton(
+                  padding: EdgeInsets.zero,
+                  onPressed: () {},
+                  child: Text(
+                    'Esquecei minha senha',
+                    textAlign: TextAlign.right,
+                  ),
+                )),
+            SizedBox(height: 16.0),
+            SizedBox(
+              height: 44.0,
+              child: RaisedButton(
+                child: Text(
+                  'Entrar',
+                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                ),
+                color: primaryColor,
+                textColor: Colors.white,
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {
+                  } else {
+                    setState(() {
+                      _validate = true;
+                    });
+                  }
+                },
+              ),
+            )
+          ],
+        ),
+      ),
     );
+  }
+
+  String _validateEmail(String text) {
+    if (text.length == 0 || !text.contains('@')) {
+      return 'E-mail Inválido';
+    } else {
+      return null;
+    }
+  }
+
+  String _validateSenha(String text) {
+    if (text.length == 0) {
+      return 'Preencha o Campo';
+    }
+    if (text.length < 6) {
+      return 'A senha deve ter no minimo 6 caracteres';
+    } else {
+      return null;
+    }
   }
 }
