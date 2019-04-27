ec4a608253ec12cbadcc9fe2e3fb4d92f93609d9
guilhermeportela <38728633+guischpor@users.noreply.github.com>
2019-04-27 17:40:40 -0300

    update

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -33,6 +33,10 @@ class _LoginScreenState extends State<LoginScreen> {
                   onPressed: () {},
                   color: Color.fromARGB(255, 0, 134, 209),
                   textColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(10.0),
+                          topLeft: Radius.circular(10.0))),
                   child: Text('SIGN IN'),
                 ),
               ),
@@ -43,13 +47,17 @@ class _LoginScreenState extends State<LoginScreen> {
                   onPressed: () {},
                   color: Colors.white,
                   textColor: Color.fromARGB(255, 0, 134, 209),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                          bottomRight: Radius.circular(10.0),
+                          topRight: Radius.circular(10.0))),
                   child: Text('SIGN IN'),
                 ),
               )
             ],
           ),
           left: (MediaQuery.of(context).size.width / 2) - 134.0,
-          top: topWidgetHeight,
+          top: topWidgetHeight + 5,
         ),
       ],
     ));
