c57b96facdbb0497b93626aef165a1c6f9719af9
guilhermeportela <38728633+guischpor@users.noreply.github.com>
2019-04-27 16:05:15 -0300

    update

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  LoginScreen({Key key}) : super(key: key);
+
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green,
+    );
+  }
+}
